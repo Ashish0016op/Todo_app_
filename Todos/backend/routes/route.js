d8a74244286d2d0e2adad02f4434ccbdbbd4ecc5
Ashish kumar <115461916+Ashish0016op@users.noreply.github.com>
2025-02-19 19:05:43 +0530

    Update route.js

--- Todos/backend/routes/route.js
+++ Todos/backend/routes/route.js
@@ -3,5 +3,8 @@ const router=express.Router();
 const userController=require('../Controllers/controller.js');
 router.post('/postData',userController.PostUser);
 router.post('/Login',userController.LoginUser);
+router.get('/todos',userController.getAllTodo);
 router.post('/addTodo',userController.addTodo);
+router.put('/updateTodo/:id',userController.updateTask);
+router.delete('/deleteTodo/:id',userController.deleteTask);
 module.exports=router;
